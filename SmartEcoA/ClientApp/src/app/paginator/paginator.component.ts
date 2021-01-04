339a24d8817e71b727b195fa575e54de0a0ead0c
Nurym Kenzh <nak290284@gmail.com>
2021-01-04 23:12:08 +0600

    Paginator: Translates

--- SmartEcoA/ClientApp/src/app/paginator/paginator.component.ts
+++ SmartEcoA/ClientApp/src/app/paginator/paginator.component.ts
@@ -5,14 +5,22 @@ import { TranslateService } from '@ngx-translate/core';
 @Injectable()
 export class Paginator extends MatPaginatorIntl {
   itemsPerPageLabel: string;
+  firstPageLabel: string;
+  previousPageLabel: string;
+  nextPageLabel: string;
+  lastPageLabel: string;
 
   constructor(translate: TranslateService,
     @Inject(LOCALE_ID) protected locale: string) {
     super();
     translate.setDefaultLang(locale);
     translate.use(locale);
-    translate.get('Paginator.ItemsPerPage').subscribe((res: string) => {
-      this.itemsPerPageLabel = res;
+    translate.get('Paginator').subscribe((res: any) => {
+      this.itemsPerPageLabel = res.ItemsPerPage;
+      this.firstPageLabel = res.FirstPageLabel;
+      this.previousPageLabel = res.PreviousPageLabel;
+      this.nextPageLabel = res.NextPageLabel;
+      this.lastPageLabel = res.LastPageLabel;
       this.getRangeLabel = (page: number, pageSize: number, length: number) => {
         if (length == 0 || pageSize == 0) { return `0 / ${length}`; }
         length = Math.max(length, 0);
