af123cc4355d07150db449608be4257511c15435
Nurym Kenzh <nak290284@gmail.com>
2021-01-30 19:06:29 +0600

    Dashboard

--- SmartEcoA/ClientApp/src/app/dashboard/dashboard.component.ts
+++ SmartEcoA/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,39 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 import { OLService } from '../ol/ol.service';
 import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements AfterViewInit {
   map: Map;
+  background = new FormControl('OSM');
 
   constructor(private olservice: OLService) { }
 
-  ngOnInit() {
-    //this.olmap();
+  ngAfterViewInit() {
     this.olservice.olmap();
   }
 
-  //private olmap() {
-  //  this.map = new Map({
-  //    target: 'map',
-  //    layers: [
-  //      new TileLayer({
-  //        source: new OSM()
-  //      })
-  //    ],
-  //    view: new View({
-  //      center: [0, 0],
-  //      zoom: 2
-  //    })
-  //  });
-  //  setTimeout(() => { this.map.updateSize(); });
-  //}
+  changeBackground(background) {
+    this.olservice.changeBackground(background);
+  }
 }
