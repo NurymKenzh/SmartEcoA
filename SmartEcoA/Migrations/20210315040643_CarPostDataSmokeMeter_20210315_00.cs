397a868f5f81982b2ca376844667a3daf133aaed
Nurym Kenzh <nak290284@gmail.com>
2021-03-15 10:17:45 +0600

    CarPostDataSmokeMeter: -> nullable properties

---
+++ SmartEcoA/Migrations/20210315040643_CarPostDataSmokeMeter_20210315_00.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartEcoA.Migrations
+{
+    public partial class CarPostDataSmokeMeter_20210315_00 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NDMax",
+                table: "CarPostDataSmokeMeter",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NDFree",
+                table: "CarPostDataSmokeMeter",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DMax",
+                table: "CarPostDataSmokeMeter",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DFree",
+                table: "CarPostDataSmokeMeter",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NDMax",
+                table: "CarPostDataSmokeMeter",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "NDFree",
+                table: "CarPostDataSmokeMeter",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DMax",
+                table: "CarPostDataSmokeMeter",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DFree",
+                table: "CarPostDataSmokeMeter",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}
