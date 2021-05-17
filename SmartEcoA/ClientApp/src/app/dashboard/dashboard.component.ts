a0c15dba697d87e9120fe7ca5e5d15815185280e
Nurym Kenzh <nak290284@gmail.com>
2021-05-17 12:33:21 +0600

    dashboard: carPosts style

--- SmartEcoA/ClientApp/src/app/dashboard/dashboard.component.ts
+++ SmartEcoA/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,7 @@ import VectorLayer from 'ol/layer/Vector';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import * as olProj from 'ol/proj';
+import { Icon, Style, Text, Fill, Stroke } from 'ol/style';
 
 @Component({
   selector: 'app-dashboard',
@@ -53,10 +54,56 @@ export class DashboardComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.olservice.olmap();
 
+    var createCarPostTextStyle = function (feature) {
+      var align = 'center';
+      var baseline = 'middle';
+      var size = '12px';
+      var height = 1;
+      var offsetX = 0;
+      var offsetY = -12;
+      var weight = 'normal';
+      var placement = 'point';
+      var maxAngle = undefined;
+      var overflow = false;
+      var rotation = 0;
+      var font = weight + ' ' + size + '/' + height + ' ' + 'Arial';
+      var fillColor = '#000000';
+      var outlineColor = '#ffffff';
+      var outlineWidth = 3;
+
+      return new Text({
+        textAlign: align == '' ? undefined : align,
+        textBaseline: baseline,
+        font: font,
+        text: feature.get('Name'),
+        fill: new Fill({ color: fillColor }),
+        stroke: new Stroke({ color: outlineColor, width: outlineWidth }),
+        offsetX: offsetX,
+        offsetY: offsetY,
+        placement: placement,
+        maxAngle: maxAngle,
+        overflow: overflow,
+        rotation: rotation,
+      });
+    };
+
+    function carPostStyleFunction(feature) {
+      return new Style({
+        image: new Icon({
+        anchor: [0.5, 0.5],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        src: '../images/icons/outline_emoji_transportation_black_24dp.png',
+      }),
+        text: createCarPostTextStyle(feature),
+      });
+    }
+
     this.olservice.map
     var Source_select_pasturepol = new VectorSource({});
     var Layer_select_pasturepol = new VectorLayer({
-      source: Source_select_pasturepol
+      source: Source_select_pasturepol,
+      style: carPostStyleFunction,
     });
     this.olservice.map.addLayer(Layer_select_pasturepol);
 
@@ -85,8 +132,9 @@ export class DashboardComponent implements AfterViewInit {
           var polyFeature = new Feature({
             geometry: new Point(olProj.transform([value.Longitude, value.Latitude], 'EPSG:4326', 'EPSG:3857')),
             id: value.Id,
-            name: value.Name
+            Name: value.Name
           });
+
           Source_select_pasturepol.addFeature(polyFeature);
         });
       });
